f7233550a312de1e7cff26865d07e2e028d6ec94
Mikoliuk Dmitry <fridoleen@gmail.com>
2021-08-04 19:40:36 +0300

    Fixed mistakes

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -18,7 +18,9 @@ namespace WishList.Controllers
         }
         public IActionResult Index()
         {
-            return View("Index");
+            var model = new List<Item>(_context.Items);
+
+            return View("Index", model);
         }
 
         [HttpGet]
@@ -35,8 +37,10 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
