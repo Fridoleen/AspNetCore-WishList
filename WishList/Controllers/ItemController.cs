36517d2dffe86afb72b2831ef3fd1c5892c1cdc9
Mikoliuk Dmitry <fridoleen@gmail.com>
2021-08-04 19:43:24 +0300

    new List() => ToList()

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -18,7 +18,7 @@ namespace WishList.Controllers
         }
         public IActionResult Index()
         {
-            var model = new List<Item>(_context.Items);
+            var model = _context.Items.ToList();
 
             return View("Index", model);
         }
