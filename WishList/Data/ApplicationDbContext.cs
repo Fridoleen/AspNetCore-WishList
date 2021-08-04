623b336130105d1e765e94efc689513102143240
Mikoliuk Dmitry <fridoleen@gmail.com>
2021-08-04 12:04:35 +0300

    Try this one, without <Db>

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -10,7 +10,6 @@ namespace WishList.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {        }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
     }
 }
